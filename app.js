6f778ee956b9642f90dd264c8811d4e16ebbd003
CarrieShort <carrie.r.phillips@gmail.com>
2016-02-15 08:35:41 -0800

    Add multiply function

--- app.js
+++ app.js
@@ -19,3 +19,8 @@ function sum(num1,num2){
   var total = num1 + num2;
   console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + total + '.');
 }
+
+function multiply(num1,num2) {
+  var total = num1 * num2;
+  console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + total + '.');
+}
